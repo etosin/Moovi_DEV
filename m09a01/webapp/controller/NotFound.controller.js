425f9b5d0c699e21197fbbece8fa3ab3a1909745
Emerson Alves Tosin <emerson.alves.tosin@inetum.com>
2023-12-11 20:51:19 +0000

    New4

--- m09a01/webapp/controller/NotFound.controller.js
+++ m09a01/webapp/controller/NotFound.controller.js
@@ -1,16 +1,13 @@
 sap.ui.define(
-    [
-        "sap/ui/core/mvc/Controller"
-    ],
-    function(Controller) {
-      "use strict";
-  
-      return Controller.extend("moovi.m09a01.controller.Notfound", {
-        onInit: function() {
-        },
-        onNavback: function(){
-            this.getRouter().navTo("RouteView1");
-        }
-      });
-    }
-  );
+  [
+    "moovi/controller/BaseController"
+  ],
+  function (Controller) {
+    "use strict";
+
+    return Controller.extend("moovi.m09a01.controller.Notfound", {
+      onInit: function () {
+      }
+
+    });
+  });
--- m09a01/webapp/controller/View1.controller.js
+++ m09a01/webapp/controller/View1.controller.js
@@ -1,5 +1,5 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "moovi/controller/BaseController"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
