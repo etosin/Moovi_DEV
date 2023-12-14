b07bc51e99d801ef24b7c4d98baa7061458e19d8
Emerson Alves Tosin <emerson.alves.tosin@inetum.com>
2023-12-14 19:51:05 +0000

    14/12

--- m09a01/webapp/controller/CompanyDetail.controller.js
+++ m09a01/webapp/controller/CompanyDetail.controller.js
@@ -1,21 +1,41 @@
 sap.ui.define(
-    [
-      "moovi/m09a01/controller/BaseController"
-    ],
-    function (Controller) {
-      "use strict";
-  
-      return Controller.extend("moovi.m09a01.controller.CompanyDetail", {
-        onInit: function () {
-            var oRouter = this.getRouter();
-            oRouter.getRoute("RouteCompanyDetail").attachMatched(this.onRouteMatched, this);
-        },
+  [
+    "moovi/m09a01/controller/BaseController"
+  ],
+  function (Controller) {
+    "use strict";
 
-        onRouteMatched: function(oEvent){
-          var oArgs, oView;
-          oArgs = oEvent.getParameter("arguments");
-          oView = this.getView();
+    return Controller.extend("moovi.m09a01.controller.CompanyDetail", {
+      onInit: function () {
+          var oRouter = this.getRouter();
+          oRouter.getRoute("RouteCompanyDetail").attachMatched(this.onRouteMatched, this);
+      },
+
+      onRouteMatched: function(oEvent){
+        var oArgs, oView;
+        oArgs = oEvent.getParameter("arguments");
+        oView = this.getView();
+
+        oView.bindElement({
+          path: "/Scarset('" + oArgs.carrId + "')",
+          events: {
+            change: this._oBindingChange.bind(this),
+            dataRequested: function (oEvent) {
+              oView.setBusy(true);
+            },
+            dataReceived: function (oEvent) {
+              oView.seBusy(false);
+            }
+          }
+        });
+      },
+      _oBindingChange: function(oEvent){
+        //No data for the binding
+        if (!this.getView().getBindingContext()) {
+             this.getRouter().getTargets().display("TargetNotFound");
         }
-  
-      });
+
+      }
+
     });
+  });
