527639cf3013a9e9360e8e2dd091b5d444b14d09
Emerson Alves Tosin <emerson.alves.tosin@inetum.com>
2023-12-15 16:00:48 +0000

    15/12

--- m09a01/webapp/test/integration/opaTests.qunit.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-
-sap.ui.require(["moovi/m09a01/test/integration/AllJourneys"
-], function () {
-	QUnit.config.autostart = false;
-	QUnit.start();
-});
--- m09a01/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"moovi/m09a01/test/unit/controller/View1.controller"
-], function () {
-	"use strict";
-});
